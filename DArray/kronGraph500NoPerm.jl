497e50356dfa596dc5c682201e08f28b203a8dfc
Andreas Noack <andreasnoackjensen@gmail.com>
2016-07-08 13:19:44 -0400

    Add threaded version of Kron

--- DArray/kronGraph500NoPerm.jl
+++ DArray/kronGraph500NoPerm.jl
@@ -114,7 +114,6 @@ function kronGraph500NoPerm3(scl, EdgesPerVertex)
     return ij1, ij2
 end
 
-# FASTEST VERSION
 # change loop order to reduce loads and stores
 function kronGraph500NoPerm4(scl, EdgesPerVertex)
 # Graph500NoPerm: Generates graph edges using the same 2x2 Kronecker algorithm (R-MAT) as the Graph500 benchmark, but no permutation of vertex labels is performed.
@@ -141,7 +140,7 @@ function kronGraph500NoPerm4(scl, EdgesPerVertex)
     a_norm = a/(a + b)
 
     randbuf = Array(Float64, 2scl)
-    @inbounds for i = 1:m
+    for i = 1:m
         rand!(randbuf)
         ij1_i = one(eltype(ij1))
         ij2_i = one(eltype(ij2))
@@ -154,6 +153,130 @@ function kronGraph500NoPerm4(scl, EdgesPerVertex)
         end
         ij1[i] = ij1_i
         ij2[i] = ij2_i
+    end
+
+    return ij1, ij2
+end
+
+# FASTEST VERSION
+# With threading
+function kronGraph500NoPerm5(scl, EdgesPerVertex)
+# Graph500NoPerm: Generates graph edges using the same 2x2 Kronecker algorithm (R-MAT) as the Graph500 benchmark, but no permutation of vertex labels is performed.
+# IO user function.
+#   Usage:
+#     StartVertex EndVertex = Graph500NoPerm(scl, edgefactor)
+#   Inputs:
+#     scl = integer scale factor that sets the max number of vertices to 2^scl
+#     EdgesPerVertex = sets the total number of edges to M = K*N;
+#  Outputs:
+#     StartVertex = M vector of integer start vertices in the range [1,N]
+#     EndVertex = M vector of integer end vertices in the range [1,N]
+
+    n = 2^scl                             # Set  power of number of vertices..
+
+    m = EdgesPerVertex * n                # Compute total number of edges to generate.
+
+    a, b, c = 0.57, 0.19, 0.19
+    d = 1 - (a + b + c)                   # Set R-MAT (2x2 Kronecker) coefficeints.
+
+    ij1, ij2 = Array(Int, m), Array(Int, m) # Initialize index arrays.
+    ab = a + b                            # Normalize coefficients.
+    c_norm = c/(1 - (a + b))
+    a_norm = a/(a + b)
+
+    @assert m % Threads.nthreads() == 0
+    Base.Threads.@threads for l = 1:Threads.nthreads()
+        # ccall(:jl_, Void, (Any,), Threads.threadid())
+        r = Base.MersenneTwister(Threads.threadid())
+        randbuf = Array(Float64, 2scl)
+        range = 1:div(m, Threads.nthreads())
+        lrange = length(range)*(Threads.threadid() - 1) + range
+        for i = lrange
+            # ccall(:jl_, Void, (Any,), i)
+            # tl_randbuf = randbuf[Threads.threadid()]
+            rand!(r, randbuf)
+            ij1_i = one(eltype(ij1))
+            ij2_i = one(eltype(ij2))
+            @inbounds for ib = 1:scl                   # Loop over each scale.
+                sc = 1 << (ib - 2)
+                ii_bit  = randbuf[ib] > ab
+                jj_bit  = randbuf[ib+scl] > ifelse(ii_bit, c_norm, a_norm)
+                # ii_bit  = rand() > ab
+                # jj_bit  = rand() > ifelse(ii_bit, c_norm, a_norm)
+                ij1_i += sc * ii_bit
+                ij2_i += sc * jj_bit
+            end
+            ij1[i] = ij1_i
+            ij2[i] = ij2_i
+        end
+    end
+
+    return ij1, ij2
+end
+
+# With threading
+function kronGraph500NoPerm6(scl, EdgesPerVertex)
+# Graph500NoPerm: Generates graph edges using the same 2x2 Kronecker algorithm (R-MAT) as the Graph500 benchmark, but no permutation of vertex labels is performed.
+# IO user function.
+#   Usage:
+#     StartVertex EndVertex = Graph500NoPerm(scl, edgefactor)
+#   Inputs:
+#     scl = integer scale factor that sets the max number of vertices to 2^scl
+#     EdgesPerVertex = sets the total number of edges to M = K*N;
+#  Outputs:
+#     StartVertex = M vector of integer start vertices in the range [1,N]
+#     EndVertex = M vector of integer end vertices in the range [1,N]
+
+    n = 2^scl                             # Set  power of number of vertices..
+
+    m = EdgesPerVertex * n                # Compute total number of edges to generate.
+
+    a, b, c = 0.57, 0.19, 0.19
+    d = 1 - (a + b + c)                   # Set R-MAT (2x2 Kronecker) coefficeints.
+
+    ij1, ij2 = Array(Int, m), Array(Int, m) # Initialize index arrays.
+    ab = a + b                            # Normalize coefficients.
+    c_norm = c/(1 - (a + b))
+    a_norm = a/(a + b)
+
+    @assert m % Threads.nthreads() == 0
+    gij1 = Array(Array{Float64, 1}, Threads.nthreads())
+    gij2 = Array(Array{Float64, 1}, Threads.nthreads())
+    Base.Threads.@threads for l = 1:Threads.nthreads()
+        # ccall(:jl_, Void, (Any,), Threads.threadid())
+        r = Base.MersenneTwister(Threads.threadid())
+        randbuf = Array(Float64, 2scl)
+        range = 1:div(m, Threads.nthreads())
+        lrange = length(range)*(Threads.threadid() - 1) + range
+        lij1 = Array(Int, length(range))
+        lij2 = Array(Int, length(range))
+        for i = range
+            # ccall(:jl_, Void, (Any,), i)
+            # tl_randbuf = randbuf[Threads.threadid()]
+            rand!(r, randbuf)
+            ij1_i = one(eltype(lij1))
+            ij2_i = one(eltype(lij2))
+            @inbounds for ib = 1:scl                   # Loop over each scale.
+                sc = 1 << (ib - 2)
+                ii_bit  = randbuf[ib] > ab
+                jj_bit  = randbuf[ib+scl] > ifelse(ii_bit, c_norm, a_norm)
+                # ii_bit  = rand() > ab
+                # jj_bit  = rand() > ifelse(ii_bit, c_norm, a_norm)
+                ij1_i += sc * ii_bit
+                ij2_i += sc * jj_bit
+            end
+            lij1[i] = ij1_i
+            lij2[i] = ij2_i
+        end
+        gij1[Threads.threadid()] = lij1
+        gij2[Threads.threadid()] = lij2
+    end
+
+    for l = 1:Threads.nthreads()
+        range = 1:div(m, Threads.nthreads())
+        lrange = length(range)*(l - 1) + range
+        ij1[range] = gij1[l]
+        ij2[range] = gij2[l]
     end
 
     return ij1, ij2
