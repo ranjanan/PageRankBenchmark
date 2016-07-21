736f3d779df4348fa0948b3fe7c02e1c402cc488
Zhang Xianyi <traits.zhang@gmail.com>
2016-07-21 22:48:42 +0800

    impl.

--- threads/generator_jthread.jl
+++ threads/generator_jthread.jl
@@ -2,62 +2,75 @@
 #
 using Base.Threads
 
+type generator_task
+    SCALE::Int
+    EdgesPerVertex::Int
+    M::Int
+    ij1::Array{Int}
+    ij2::Array{Int}
+end
+
+mytask=generator_task[]
+
 
 function generator_inner()
-SCALE=18
-EdgesPerVertex = 4 #16
-
-   # tic()
+    SCALE=18 #mytask[1].SCALE
+    EdgesPerVertex = 4 #mytask[1].EdgesPerVertex
 
   N = 2.^SCALE                       # Set  power of number of vertices..
 
   np=nthreads()
-  M = round(Int, EdgesPerVertex .* N)     # Compute total number of edges to generate.
 
-  M = div(M,np)
+  r = Base.MersenneTwister(Threads.threadid())
+
+    M = round(Int, EdgesPerVertex .* N)     # Compute total number of edges to generate.
+
+#  M = mytask[1].M
+
+    ij1=Array(Int, M)#mytask[1].ij1
+    ij2=Array(Int, M)#mytask[1].ij2
+
+  local_M = div(M,np)
+    range = 1:local_M
+    lrange=local_M*(threadid()-1) + range
 
   A = 0.57; B = 0.19;  C = 0.19;   D = 1-(A+B+C)  # Set R-MAT (2x2 Kronecker) coefficients.
-  ij = ones(Int, 2, M)         # Initialize index arrays.
-
   ab = A + B                 # Normalize coefficients.
   c_norm = C/(1 - (A + B))
   a_norm = A/(A + B)
 
-   # begin_time=toq()
-
-  #  tic()
-
-
-    for j = 1:M
-      for ib = 1:SCALE            # Loop over each scale.
+    for j = 1:local_M
+       for ib = 1:SCALE            # Loop over each scale.
           k = 1 << (ib-1)
-          if rand() > ab
-              ij[1,j] += k
-              if rand() > c_norm
-                  ij[2,j] += k
+          if rand(r) > ab
+              ij1[j] += k
+              if rand(r) > c_norm
+                  ij2[j] += k
               end
-          elseif rand() > a_norm
-              ij[2,j] += k
+          elseif rand(r) > a_norm
+              ij2[j] += k
           end
       end
   end
 
- #   loop_time=toq()
- #   tic()
-  StartVertex = view(ij,1,:)     # Copy to output. (row vector)
-  EndVertex =   view(ij,2,:)       # Copy to output. (row vector)
-#    copy_time=toq()
     return
+end
 
+function generator(SCALE,EdgesPerVertex)
+    N = 2.^SCALE                       # Set  power of number of vertices..
+    M = round(Int, EdgesPerVertex .* N)     # Compute total number of edges to generate.
+println(SCALE," ", EdgesPerVertex," " , M)
+    push!(mytask, generator_task(SCALE, EdgesPerVertex, M, Array(Int, M), Array(Int, M)))
+    ccall(:jl_threading_run, Void, (Any,), Core.svec(generator_inner))
+#    return mytask[1].ij1, mytask[1].ij2
 end
 
 println("Threads: ",nthreads())
-#generator_inner()
+
+SCALE=18
+EdgesPerVertex = 16
 tic()
-#generator_inner()
-ccall(:jl_threading_run, Void, (Any,), Core.svec(generator_inner))
+generator(SCALE, EdgesPerVertex)
 K0time=toq()
 
-
-#println("Begin Time: ", begin_time, " Loop Time: ", loop_time, " Copy time: ", copy_time)
 println("Total Time: " * string(K0time))
