942ba6bcc44d02fbcc244b001de1143ad6789826
Valentin Churavy <v.churavy@gmail.com>
2016-07-28 07:35:35 +0900

    Implement generate, write and read for DArray

--- DArray/pagerank.jl
+++ DArray/pagerank.jl
@@ -1,26 +1,40 @@
 module Pagerank
+using DistributedArrays
+import DistributedArrays: map_localparts
 
 include("kronGraph500NoPerm.jl")
 include("io.jl")
 
-function run(scl, EdgesPerVertex)
-   info("Scale:", scl)
-   info("EdgesPerVertex:", EdgesPerVertex)
-   info("Number of threads: ", Threads.nthreads())
-
+function kernel0(path, scl, EdgesPerVertes)
    info("Generating data")
-   @time ij = kronGraph500NoPerm(scl, EdgesPerVertex)
-
-   filename = "1.tsv"
+   @time edges = DArray(I->kronGraph500NoPerm(scl, EdgesPerVertes), (nworkers(), ))
 
    info("Writing data:")
-   @time writetsv(filename, ij)
+   @time map_localparts(edges) do local_edges
+      filename = joinpath(path, "$(myid()).tsv")
+      writecsv(filename, local_edges)
+   end
+end
 
+function kernel1(path)
    info("Read data")
-   @time _ij = readtsv(filename)
+   rrefs = map(workers()) do id
+      filename = joinpath(path, "$id.tsv")
+      remotecall(readtsv, id, filename)
+   end
+   edges = DArray(rrefs)
+end
 
-   @assert all(ij .== _ij)
-   ij
+function run(path, scl, EdgesPerVertex)
+   info("Scale:", scl)
+   info("EdgesPerVertex:", EdgesPerVertex)
+   info("Number of workers: ", nworkers())
+
+   info("Executing kernel 0")
+   @time kernel0(path, scl, EdgesPerVertex)
+
+   info("Executing kernel 1")
+   @time kernel1(path)
 end
 
 end
